3832b8b57a4bcd90a486eba415d1b77021e83be8
Aron Polner <aron.polner@gmail.com>
2019-09-16 16:11:35 +0200

    Places an object +1f in Z direction

--- Assets/PlacementManager.cs
+++ Assets/PlacementManager.cs
@@ -10,9 +10,11 @@ public class PlacementManager : MonoBehaviour
     private ARCameraManager arCameraManager;
     private GameObject placedObject;
 	private Touch touch;
+    private Vector3 oTransformPos;
 
 	private void Awake()
     {
+        oTransformPos = transform.position;
         arCameraManager = GetComponent<ARCameraManager>();
         arSessionOrigin = GetComponent<ARSessionOrigin>();
     }
@@ -22,18 +24,19 @@ public class PlacementManager : MonoBehaviour
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
 			Debug.Log("Touch began");
+            if(placedObject == null)
+            {
+                touch = Input.GetTouch(0);
+
+                placedObject = Instantiate(objectPrefab);
+                var positionToPlaceAt = new Vector3(oTransformPos.x, oTransformPos.y, oTransformPos.z + 1f);
+                arSessionOrigin.MakeContentAppearAt(placedObject.transform, positionToPlaceAt, Quaternion.identity);
+            }
         }
 
         else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
 			Debug.Log("Touch ended");
-            if(placedObject == null)
-            {
-                touch = Input.GetTouch(0);
-                placedObject = Instantiate(objectPrefab, touch.position, Quaternion.identity);
-                var positionToPlaceAt = arCameraManager.transform.position + Vector3.forward * 0.5f;
-                arSessionOrigin.MakeContentAppearAt(placedObject.transform, positionToPlaceAt, Quaternion.identity);
-            }
         }
     }
 
