fb9966089e319312b58e4fb1523eb03140ac799c
Aron Polner <aron.polner@gmail.com>
2019-09-16 17:40:41 +0200

    Can place an object rel. to screen touch. Can also follow the camera w placed object.

--- Assets/PlacementManager.cs
+++ Assets/PlacementManager.cs
@@ -5,33 +5,39 @@ using UnityEngine.EventSystems;
 public class PlacementManager : MonoBehaviour
 {
     [SerializeField] private GameObject objectPrefab;
+    [SerializeField] private Camera arCamera;
 
     private ARSessionOrigin arSessionOrigin;
-    private ARCameraManager arCameraManager;
     private GameObject placedObject;
 	private Touch touch;
-    private Vector3 oTransformPos;
 
 	private void Awake()
     {
-        oTransformPos = transform.position;
-        arCameraManager = GetComponent<ARCameraManager>();
         arSessionOrigin = GetComponent<ARSessionOrigin>();
     }
 
     void Update()
     {
+        // For debugging
+        //var cameraStuff = arCamera.transform.position;
+        //Debug.Log("camera transform pos: " + cameraStuff);
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
 			Debug.Log("Touch began");
             if(placedObject == null)
             {
                 touch = Input.GetTouch(0);
-
+                Vector3 pos = arCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 1f));
                 placedObject = Instantiate(objectPrefab);
-                var positionToPlaceAt = new Vector3(oTransformPos.x, oTransformPos.y, oTransformPos.z + 1f);
-                arSessionOrigin.MakeContentAppearAt(placedObject.transform, positionToPlaceAt, Quaternion.identity);
+                var pos1 = new Vector3(transform.position.x, transform.position.y, 1);
+                arSessionOrigin.MakeContentAppearAt(placedObject.transform, pos1, Quaternion.identity);
             }
+        }
+
+        else if(placedObject != null)
+        {
+            placedObject.transform.position = arCamera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 1f));
+            // rotation has to be set towards the camera
         }
 
         else if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
